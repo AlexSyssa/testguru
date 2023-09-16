329c5cf134c1255eb569711723fb095052576801
AlexSyssa <lifeloverunn@gmail.com>
2023-09-16 16:50:11 +0700

    correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'tests#index'
 
- root to: 'tests#index'
-
- resources :tests
-
- resources :questions
-
- resources :tests do
-   resources :questions, shallow: true
- end
-
- get '/show_question/:id', to: 'questions#show_question'
-
-# post '/questions/new/:id', to: 'questions#new'
-
-# get '/tests/:category/:title', to: 'tests#search', level: 2
-
- resources :questions do
-   member do
-     post :new
-   end
- end
+  resources :tests do
+    resources :questions, shallow: true
+  end
 end
