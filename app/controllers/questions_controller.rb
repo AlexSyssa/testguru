44274d846f335f03aabaa38acf7a29420e401700
AlexSyssa <lifeloverunn@gmail.com>
2023-09-17 09:01:43 +0700

    correction2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,26 +1,26 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[show]
+  before_action :find_test, only: %i[index creste]
+  before_action :find_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 #все вопросы конкретного теста
   def index
-    render json: {questions: Question.where(test_id: params[:test_id])}
+    render json: find_test.questions
   end
 #просмотр конкретного вопроса теста
   def show
-    render json: {questions: Question.find(params[:id])}
+    render json: find_question
   end
 
   def destroy
-    @question = Question.find(params[:id])
-    @question.destroy
+    find_question.destroy
   end
 
   def new
   end
 
   def create
-    @question = Question.new(test_id: params[:test_id], body: params.permit(:body))
+    @question = find_test.questions.new(body: params.permit(:body))
     if @question.save
       render inline: '<p>Question: <%= @question.body %>! was save </p>'
     else
@@ -31,7 +31,11 @@ class QuestionsController < ApplicationController
   private
 
   def find_test
-    @test = Test.find(params[:id])
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def rescue_with_question_not_found
