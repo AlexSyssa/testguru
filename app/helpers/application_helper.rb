20f81291ad02ec8ddba5b7982d2ee964f4473b19
AlexSyssa <lifeloverunn@gmail.com>
2023-09-24 00:47:46 +0700

    views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def current_year
+    Date.today.year
+  end
+
+  def githab_url(author, repo)
+    link_to repo, "https://github.com/#{author}/#{repo}", target: :blank
+  end
 end
