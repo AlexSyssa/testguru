176c07e5979a2ecea8438d2a9605e5429dd2b6e9
AlexSyssa <lifeloverunn@gmail.com>
2023-10-08 22:17:13 +0700

    callback

---
+++ app/models/test_passage.rb
@@ -0,0 +1,68 @@
+class TestPassage < ApplicationRecord
+  SUCCESSFUL_RESULT = 85.freeze
+
+  belongs_to :user
+  belongs_to :test
+  belongs_to :current_question, class_name: 'Question', optional: true
+
+  before_validation :before_validation_set_current_question, on: :create
+  before_update :set_next_question
+
+  def successful?
+    correct_percent >= SUCCESSFUL_RESULT
+  end
+
+  def completed?
+    current_question.nil?
+  end
+
+  def complete_test
+    self.current_question = nil
+  end
+
+  def correct_percent
+    (100.00 * correct_questions) / test.questions.count
+  end
+
+  def accept!(answer_ids)
+    if correct_answer?(answer_ids)
+      self.correct_questions += 1
+    end
+
+    save!
+  end
+
+  def current_question_number
+    test.questions.order(:id).where('id <= ?', (current_question.id)).count
+  end
+
+  def set_next_question
+    self.current_question = next_question
+  end
+
+  private
+
+  def before_validation_set_current_question
+    self.current_question = test.questions.first if test.present?
+  end
+
+  def next_question
+    test.questions.order(:id).where('id > ?', current_question.id).first
+  end
+
+  # def next_question
+  #   self.current_question = if new_record?
+  #     test.questions.order(:id).first if test.present?
+  #   else
+  #     test.questions.order(:id).where('id > ?', current_question.id).first
+  #   end
+  # end
+
+  def correct_answer?(answer_ids)
+    correct_answers.ids.sort == answer_ids.to_a.map(&:to_i).sort
+  end
+
+  def correct_answers
+    current_question.answers.correct
+  end
+end
