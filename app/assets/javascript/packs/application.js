2f68e0486603a0203f02ec7ebcfccd04f84aa886
AlexSyssa <lifeloverunn@gmail.com>
2023-12-16 21:44:32 +0700

    js

--- app/assets/javascript/packs/application.js
+++ app/assets/javascript/packs/application.js
@@ -2,17 +2,21 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
-require jquery3
-require popper
-require bootstrap-sprockets
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require_tree .
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("../utilities/sorting")
